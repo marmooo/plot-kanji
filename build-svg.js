a3a6d83709580c04b59f8eba94e74f6f18723e98
marmooo <marmooo@users.noreply.github.com>
2024-03-31 00:59:31 +0900

    bump @marmooo/ttf2svg from 0.1.4 to 0.1.6

--- build-svg.js
+++ build-svg.js
@@ -1,19 +1,24 @@
-import { ttf2svg } from "npm:@marmooo/ttf2svg@0.1.4";
+import { parse } from "npm:opentype.js@1.3.4";
+import { filterGlyphs, toSVG } from "npm:@marmooo/ttf2svg@0.1.6";
 import { JISCode } from "npm:@marmooo/kanji@0.0.8";
 
 function loadKanjiList() {
-  let chars = "ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんゔァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴヵ";
+  let chars =
+    "ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんゔァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴヵ";
   JISCode.forEach((list) => {
     chars += list.join("");
   });
   return chars;
 }
 
-function parseTTF(chars, inFile, outDir, options = {}) {
+function parseTTF(inFile, outDir, options = {}) {
   Deno.mkdirSync(outDir, { recursive: true });
-  const svgs = ttf2svg(inFile, chars, options);
-  for (const { glyph, svg } of svgs) {
+  const uint8array = Deno.readFileSync(inFile);
+  const font = parse(uint8array.buffer);
+  const glyphs = filterGlyphs(font, options);
+  for (const glyph of glyphs) {
     if (!glyph.unicode) continue;
+    const svg = toSVG(font, glyph, options);
     if (!svg) continue;
     const { x1 } = glyph.getBoundingBox();
     const fileName = Number(glyph.unicode).toString(16);
@@ -22,37 +27,31 @@ function parseTTF(chars, inFile, outDir, options = {}) {
       console.log("x1", x1, glyph.unicode, fileName, char);
       continue;
     }
-    // if (y1 < 0) {
-    //   console.log("y1", y1, glyph.unicode, fileName, char);
-    //   continue;
-    // }
     Deno.writeTextFileSync(`${outDir}/${fileName}.svg`, svg);
   }
 }
 
-function notoSerifJP(chars) {
+function notoSerifJP(text) {
   console.log("Noto Serif JP");
-  const options = { glyphHeight: 1000, translateY: 850 };
+  const options = { text, glyphHeight: 1000, translateY: 850 };
   parseTTF(
-    chars,
     "vendor/NotoSerifJP-Regular.otf",
     "svg/noto-serif-jp",
     options,
   );
 }
 
-function notoSansJP(chars) {
+function notoSansJP(text) {
   console.log("Noto Sans JP");
-  const options = { glyphHeight: 1000, translateY: 850 };
+  const options = { text, glyphHeight: 1000, translateY: 850 };
   parseTTF(
-    chars,
     "vendor/NotoSansJP-Regular.otf",
     "svg/noto-sans-jp",
     options,
   );
 }
 
-const chars = loadKanjiList();
-console.log(chars.length);
-notoSerifJP(chars);
-notoSansJP(chars);
+const text = loadKanjiList();
+console.log(text.length);
+notoSerifJP(text);
+notoSansJP(text);
