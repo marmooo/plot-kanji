2b6c33a075bf210ff2228db99668c40289060abb
marmooo <marmooo@users.noreply.github.com>
2025-03-22 19:35:17 +0900

    bump signature_pad from 5.0.4 to 5.0.7

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { shape2path } from "https://cdn.jsdelivr.net/npm/@marmooo/shape2path@0.0.2/+esm";
 import svgpath from "https://cdn.jsdelivr.net/npm/svgpath@2.6.0/+esm";
-import signaturePad from "https://cdn.jsdelivr.net/npm/signature_pad@5.0.4/+esm";
+import signaturePad from "https://cdn.jsdelivr.net/npm/signature_pad@5.0.7/+esm";
 
 document.getElementById("gamePanel")
   .appendChild(document.getElementById("gamePanelTemplate").content);
