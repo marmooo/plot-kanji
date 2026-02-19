ec28d7f78fcc2bfcf6964d67fad7860cc85d5fad
marmooo <marmooo@users.noreply.github.com>
2026-02-20 00:31:25 +0900

    update build script

--- build-svg.js
+++ build-svg.js
@@ -32,7 +32,7 @@ function parseTTF(inFile, outDir, options = {}) {
 
 function notoSerifJP(text) {
   console.log("Noto Serif JP");
-  const options = { text, glyphHeight: 1000, translateY: 850 };
+  const options = { text };
   parseTTF(
     "vendor/NotoSerifJP-Regular.otf",
     "svg/noto-serif-jp",
@@ -42,7 +42,7 @@ function notoSerifJP(text) {
 
 function notoSansJP(text) {
   console.log("Noto Sans JP");
-  const options = { text, glyphHeight: 1000, translateY: 850 };
+  const options = { text };
   parseTTF(
     "vendor/NotoSansJP-Regular.otf",
     "svg/noto-sans-jp",
