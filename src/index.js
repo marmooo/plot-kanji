32ba1312e462bcafdd38dba5002b9b1f539ad9f5
marmooo <marmooo@users.noreply.github.com>
2026-04-10 00:05:30 +0900

    improve dark-mode

--- src/index.js
+++ src/index.js
@@ -8,22 +8,14 @@ const emojiParticle = initEmojiParticle();
 const maxParticleCount = 10;
 let audioContext;
 const audioBufferCache = {};
-loadConfig();
-
-function loadConfig() {
-  if (localStorage.getItem("darkMode") == 1) {
-    document.documentElement.setAttribute("data-bs-theme", "dark");
-  }
-}
 
 function toggleDarkMode() {
-  if (localStorage.getItem("darkMode") == 1) {
-    localStorage.setItem("darkMode", 0);
-    document.documentElement.setAttribute("data-bs-theme", "light");
-  } else {
-    localStorage.setItem("darkMode", 1);
-    document.documentElement.setAttribute("data-bs-theme", "dark");
-  }
+  const html = document.documentElement;
+  const newTheme = html.getAttribute("data-bs-theme") === "dark"
+    ? "light"
+    : "dark";
+  html.setAttribute("data-bs-theme", newTheme);
+  localStorage.setItem("darkMode", newTheme);
 }
 
 function createAudioContext() {
--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const cacheName = "2025-12-21 00:00";
+const cacheName = "2026-04-10 00:00";
 const urlsToCache = [
   "/plot-kanji/index.js",
   "/plot-kanji/mp3/boyon1.mp3",
